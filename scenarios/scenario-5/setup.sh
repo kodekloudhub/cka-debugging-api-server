3afbc9145809f9f1c5bc15a1778cab2bca2cf194
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2023-02-28 05:59:10 +0000

    Add hint text

--- scenarios/scenario-5/setup.sh
+++ scenarios/scenario-5/setup.sh
@@ -26,3 +26,15 @@ yq -i e '.spec.containers[0].command += [
     .spec.containers[0].volumeMounts += {"mountPath": "/etc/kubernetes/prod-audit.yaml", "name": "audit", "readOnly": true } |
     .spec.containers[0].volumeMounts += {"mountPath": "/var/log/prod-secrets.log", "name": "audit-log"}' \
     /etc/kubernetes/manifests/kube-apiserver.yaml
+
+echo
+echo "Hint"
+echo "This scenario is configuring APi server auditing. This involves:"
+echo
+echo "* A file /etc/kubernetes/prod-audit.yaml has been created."
+echo "* This file should be correctly mounted as a volume by API server."
+echo "* The file is read by api server argument --audit-policy-file."
+echo
+echo "If you are a CKA student, you don't need to know the details of"
+echo "auditing to solve this challenge."
+echo
